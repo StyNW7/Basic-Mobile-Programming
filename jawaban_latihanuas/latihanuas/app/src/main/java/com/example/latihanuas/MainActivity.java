ee4d7cb4c05c512c277851957ea95876e4e3cc94
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2026-01-17 17:17:56 +0700

    feat: add latihan uas mobprog 2026

---
+++ jawaban_latihanuas/latihanuas/app/src/main/java/com/example/latihanuas/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.latihanuas;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.latihanuas.fragments.DanceFragment;
+import com.example.latihanuas.fragments.DdolFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        //TODO 1A: by default tampilin DdolFragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, new DdolFragment())
+                .commit();
+
+        //TODO 1B: atur kalau tiap menu di klik, tampilkan fragment yang sesuai
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.menu_ddol) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, new DdolFragment())
+                        .commit();
+            } else if (item.getItemId() == R.id.menu_dance) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, new DanceFragment())
+                        .commit();
+            }
+            return false;
+        });
+
+    }
+}
---
+++ jawaban_latihanuas/latihanuas/app/src/main/java/com/example/latihanuas/services/PlayBabySharkService.java
@@ -0,0 +1,45 @@
+package com.example.latihanuas.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.latihanuas.R;
+
+public class PlayBabySharkService extends Service {
+    private MediaPlayer mediaPlayer;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //TODO 3A: play baby shark dari R.raw di sini
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.mybaby_shark);
+            mediaPlayer.setLooping(true); //optional, biar muter terus
+            mediaPlayer.start();
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //TODO 3B: stop media player
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release(); //WAJIB dipanggil supaya tidak memory leak
+            mediaPlayer = null;
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
